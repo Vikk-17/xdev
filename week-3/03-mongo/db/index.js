2d1f76ace833601858a8327173c1d8d67a5bcede
Souvik Chakraborty <Souvikc3030@gmail.com>
2024-08-15 22:30:44 +0530

    update

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+const uri = "mongodb+srv://souvikc3030:ZGiFS8AauyOKQnUg@cluster0.dn6qkrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(uri); // mongodb+srv://souvikc3030:<password>@cluster0.dn6qkrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourse: [{
+        type: mongoose.Schema.type.ObjectId,
+        ref: 'Course',
+    }],
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    prince: Number,
+    imageLink: String,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
